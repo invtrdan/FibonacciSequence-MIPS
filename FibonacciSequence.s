ec65c481b6507c18b30d21835160d56aea67659b
Danielle McIntosh <118628680+invtrdan@users.noreply.github.com>
2022-12-04 01:30:02 -0500

    Update FibonacciSequence.s
    - The syscall  instruction triggers a system call exception (exception code 8) that causes control to be transferred from user space to kernel space where the system call is handled.
    - print is stored in the address $a0

--- FibonacciSequence.s
+++ FibonacciSequence.s
@@ -1,11 +1,14 @@
 #Author: Danielle McIntosh
 
-.data                                                           # Define the program data
-promptN: .asciiz "\nEnter an integer N, where N >= 25: "        # N Prompt
-promptFib: .asciiz "\nFibonacci Sequence"                       # Fibonacci Sequence Prompt
+.data                                                          # Define the program data
+promptN: .asciiz "\nEnter an integer N, where N >= 25: "       # N Prompt
+promptFib: .asciiz "\nFibonacci Sequence"                      # Fibonacci Sequence Prompt
 
-.text                                                           # Define the program instructions
-main:                                                           # Starting point 
-  li $v0, 4                                                     # Load 4 into $v0 to indicate a print string.
-  la $a0, promptN                                               # Load the address of pronptN into $a0
-  syscall                                                       # This is where the program ends
+.text                                                          # Define the program instructions
+main:                                                          # Starting point 
+
+  li $v0, 4                                                    # Load 4 into $v0 to indicate a print string.
+  la $a0, promptN                                              # Load the address of pronptN into $a0
+  syscall                                                      # Control is transferred from the user space to the kernel where the system call is handled
+  
+  syscall                                                      # This is where the program ends

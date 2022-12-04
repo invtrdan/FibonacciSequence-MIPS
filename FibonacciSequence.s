a531f96a30413837ee5edc76eb3b7b077d25400f
Danielle McIntosh <118628680+invtrdan@users.noreply.github.com>
2022-12-04 02:59:48 -0500

    Update FibonacciSequence.s
    - li $v0, 5 reads the input integer and stores it at $v0
    - move $s0, $v0 saves the integer in $s0.

--- FibonacciSequence.s
+++ FibonacciSequence.s
@@ -9,5 +9,9 @@ main:                                                          # Starting point
   li $v0, 4                                                    # Load 4 into $v0 to indicate a print string.
   la $a0, promptN                                              # Load the address of pronptN into $a0
   syscall                                                      # Control is transferred from the user space to the kernel where the system call is handled
-  li $v0, 5                                                    # Read the input integer
-  syscall                                                      # This is where the program ends
+  li $v0, 5                                                    # Read the input integer, stores it at $v0
+  syscall                                                      # Request system service
+  move $s0, $v0                                                #Save the integer in $s0
+  
+  
+ 

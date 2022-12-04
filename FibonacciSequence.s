d089448c288cff3dc3852979e2b8bc84238964fa
Danielle McIntosh <118628680+invtrdan@users.noreply.github.com>
2022-12-04 01:16:32 -0500

    Update FibonacciSequence.s
    - The label main labels the starting point.
    - li $v0, 4 is used to load 4 into $v0 to indicate a print string.
    - la $a0, promptN loads the address of promptN into $a0.
    - sys call is where the program ends.

--- FibonacciSequence.s
+++ FibonacciSequence.s
@@ -1,9 +1,11 @@
 #Author: Danielle McIntosh
 
-.data
-promptN: .asciiz "\nEnter an integer N, where N >= 25: "
-promptFib: .asciiz "\nFibonacci Sequence"
+.data                                                           # Define the program data
+promptN: .asciiz "\nEnter an integer N, where N >= 25: "        # N Prompt
+promptFib: .asciiz "\nFibonacci Sequence"                       # Fibonacci Sequence Prompt
 
-.text
-main:
-  #Prompt the user to enter an integer N, where N >= 25
+.text                                                           # Define the program instructions
+main:                                                           # Starting point 
+  li $v0, 4                                                     # Load 4 into $v0 to indicate a print string.
+  la $a0, promptN                                               # Load the address of pronptN into $a0
+  syscall                                                       # This is where the program ends

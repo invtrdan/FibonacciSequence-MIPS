630e1ff16feff14d490c70a5c5342668a62930d6
Danielle McIntosh <118628680+invtrdan@users.noreply.github.com>
2022-12-04 04:29:20 -0500

    Update FibonacciSequence.s
    Added the missing colons on lines 6 and 7.

--- FibonacciSequence.s
+++ FibonacciSequence.s
@@ -3,8 +3,8 @@
 .data                                                          # Define the program data
 promptN: .asciiz "\nEnter an integer N, where N >= 25: "       # N Prompt
 promptFib: .asciiz "\nFibonacci Sequence"                      # Fibonacci Sequence Prompt
-is_legal .asciiz "\nThe number is legal"                       #Variable that signifies that the number is legal
-is_illegal .asciiz "\nIllegal Number!"                         #Variable that signifies that the number is illegal
+is_legal: .asciiz "\nThe number is legal"                       #Variable that signifies that the number is legal
+is_illegal: .asciiz "\nIllegal Number!"                         #Variable that signifies that the number is illegal
 
 .text                                                          # Define the program instructions
 main:                                                          # Starting point 
@@ -14,17 +14,17 @@ main:                                                          # Starting point
   li $v0, 5                                                    # Read the input integer, stores it at $v0
   syscall                                                      # Request system service
   move $8, $v0                                                 # Save the integer in $s0 (a temporary register)
-  blt $8, 25, isIllegal                                        #If the number that the user enters is less than 25, branch to isIllegal
-  beq $8, 25, isLegal                                          #If the number that the user enters is 25, branch to isLegal
+  blt $8, 25, isIllegal                                        # If the number that the user enters is less than 25, branch to isIllegal
+  beq $8, 25, isLegal                                          # If the number that the user enters is 25, branch to isLegal
   
 isLegal:
-  li $v0, 4                                                    #Print("The number is legal")
-  la $a0, is_illegal                                           #log the address of is_legal
+  li $v0, 4                                                    # Print("The number is legal")
+  la $a0, is_illegal                                           # log the address of is_legal
   syscall
 
 isIllegal:
-  li $v0, 4                                                    #Print("Illegal Number!")
-  la $a0, is_illegal                                           #log the address of is_illegal
+  li $v0, 4                                                    # Print("Illegal Number!")
+  la $a0, is_illegal                                           # log the address of is_illegal
   syscall
 
 fib:
